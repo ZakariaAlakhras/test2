4a375036767e8d2dcf5f2328f8bf0d687f4215ef
ZakariaAlakhras <zakaria@eremnews.com>
2017-10-13 12:11:26 +0300

    add new data on lable

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,7 +14,8 @@ public partial class _Default : System.Web.UI.Page
     protected void Button1_Click(object sender, EventArgs e)
     {
         TextBox1.Text = "hello zak";
-        Label1.Text ="Say Hello is done ";
+        Label1.Text ="Say Hello is done okay ";
+        Label1.Text = Label1.Text + " zak";
 
     }
 }
